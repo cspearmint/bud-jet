0529233ce0dac68d6a4caa022aeed479d65c1f3f
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2024-03-25 15:50:17 -0400

    updated app.js for page routing

--- budjet/src/App.js
+++ budjet/src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "@fontsource/inter"; // Import fontsource/inter
+import "@fontsource/inter/400.css"; // Import fontsource/inter CSS
 import './App.css';
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from './components/home.js'; 
+import Main from './components/main.js'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-};
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
-
