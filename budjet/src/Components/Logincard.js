bb9fb005f7ad452f11d1757d366d8378953273f5
leynarhuynh <85371413+leynarhuynh@users.noreply.github.com>
2024-03-26 11:26:43 -0400

    install meterialui

--- budjet/src/Components/Logincard.js
+++ budjet/src/Components/Logincard.js
@@ -3,30 +3,30 @@ import "./Logincard.css";
 import Heading from "./Heading";
 import Textbox from "./Textbox";
 import Checkbox from "./Checkbox";
-import Loginbutton from "./Loginbutton";
 import { Link } from 'react-router-dom';
-
-/*Fix responsiveness to change in vertical window size*/
+import Button from '@mui/material/Button'; 
 
 const Logincard = () => {
     return (
-        <div className = "login_card">
-            <div className = "login_card-container">
-                <Heading text="Login"></Heading>
-                <div className = "login_card-container-text">
-                    <p className = "login_card-container-text-p1">Don't have an account yet? <Link to="/Signup" class="highlighted-text">Sign up!</Link></p>
-                    <div className = "login_card-container-text-textboxes">
-                        <p className = "login_card-container-text-textboxes-p3">Email Address:</p>
+        <div className="login_card">
+            <div className="login_card-container">
+                <Heading text="Login" />
+                <div className="login_card-container-text">
+                    <p className="login_card-container-text-p1">Don't have an account yet? <Link to="/Signup" className="highlighted-text">Sign up!</Link></p>
+                    <div className="login_card-container-text-textboxes">
+                        <p className="login_card-container-text-textboxes-p3">Email Address:</p>
                         <Textbox />
-                        <p className = "login_card-container-text-textboxes-p3">Password:</p>
+                        <p className="login_card-container-text-textboxes-p3">Password:</p>
                         <Textbox />
                     </div>
                     <Checkbox />
-                    <Loginbutton text="Login" />
+                    <Button variant="contained" color="primary">
+                        Login
+                    </Button>
                 </div>
             </div>
-        </div>      
-);
-  };
-  
-  export default Logincard;
+        </div>
+    );
+};
+
+export default Logincard;
